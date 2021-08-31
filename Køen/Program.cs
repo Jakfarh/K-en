7b8f144897c0c4c9b9b159d58bdc3ad68b332648
Jakob Matthias Bek Farholt <jako741m@zbc.dk>
2021-08-31 13:10:56 +0100

    Add project files.

---
+++ Køen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Køen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool filmliste;
+            filmliste = true;
+            while (filmliste)
+            {
+                Console.WriteLine("=======================================");
+                Console.WriteLine("==============Filmlisten===============");
+                Console.WriteLine("=======================================");
+                Console.WriteLine();
+                Console.WriteLine("1, tilføj en film");
+                Console.WriteLine("2, fjern en film");
+                Console.WriteLine("3, vis antal film i køen");
+                Console.WriteLine("4, vis hvilke film der er i kø");
+                Console.WriteLine("5, afslut");
+
+                int choice = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
