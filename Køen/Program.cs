09f29d7290a87c70cc2684443cd71cf215046512
Jakob Matthias Bek Farholt <jako741m@zbc.dk>
2021-09-01 12:13:56 +0100

    Finished product, take1

--- Køen/Program.cs
+++ Køen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Køen
 {
@@ -6,10 +8,12 @@ namespace Køen
     {
         static void Main(string[] args)
         {
+            Queue<string> Filmkø = new Queue<string>();
             bool filmliste;
             filmliste = true;
             while (filmliste)
             {
+                Console.Clear();
                 Console.WriteLine("=======================================");
                 Console.WriteLine("==============Filmlisten===============");
                 Console.WriteLine("=======================================");
@@ -20,8 +24,48 @@ namespace Køen
                 Console.WriteLine("4, vis hvilke film der er i kø");
                 Console.WriteLine("5, afslut");
 
-                int choice = int.Parse(Console.ReadLine());
+
+                switchcase valg = new switchcase();
+                string result = valg.Choice(int.Parse(Console.ReadLine()));
+                Console.WriteLine(result);
+
+                if (result == "Programmet afsluttes")
+                {
+                    filmliste = false;
+                }
+
+
+
+                else if (result == "Du har valgt at tilføje en film!")
+                {
+                    Console.WriteLine("Hvad er filmens titel?");
+                    string Movie = Console.ReadLine();
+                    Console.WriteLine($"Vi har nu tilføjet {Movie} til køen!");
+                    Filmkø.Enqueue(Movie);
+                    Console.ReadLine();
+                }
+                else if (result == "Du har set den næste film i rækken, og den vil blive fjernet fra køen")
+                {
+                    Filmkø.Dequeue();
+                    Console.ReadLine();
+                }
+                else if (result == "Film i køen:")
+                {
+                    Console.WriteLine($"Film i køen: {Filmkø.Count}");
+                    Console.ReadLine();
+                }
+                else if (result== "Disse film er tilbage i køen")
+                {
+                    var Filmliste = Filmkø.ToList();
+                    foreach (var film in Filmliste)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(film);
+                    }
+                    Console.ReadLine();
+                }
             }
         }
     }
 }
+
--- Køen/switchcase.cs
+++ Køen/switchcase.cs
@@ -6,8 +6,35 @@ using System.Threading.Tasks;
 
 namespace Køen
 {
-    class switchcase
+    public class switchcase
     {
-        int choice1 = choice
+
+        public string Choice(int choice)
+        {
+            string returnvalue = "0";
+            switch (choice)
+            {
+                case 1:
+                    returnvalue = "Du har valgt at tilføje en film!";
+                    break;
+
+                case 2:
+                    returnvalue = "Du har set den næste film i rækken, og den vil blive fjernet fra køen";
+                    break;
+
+                case 3:
+                    returnvalue = "Film i køen:";
+                    break;
+
+                case 4:
+                    returnvalue = "Disse film er tilbage i køen";
+                    break;
+
+                case 5:
+                    returnvalue = "Programmet afsluttes";
+                    break;
+            }
+            return returnvalue;
+        }
     }
 }
